613792c5ca9a8545d38d2bfe0d325bf0c7a66fba
Himani Mahajan <himanimahajan2709@gmail.com>
2025-08-19 23:02:01 +0530

    Add files via upload

---
+++ app (2).py
@@ -0,0 +1,25 @@
+import pickle
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Load model
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+@app.route("/")
+def home():
+    return {"message": "API is running!"}
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        data = request.get_json()
+        features = data["features"]  # list of input values
+        prediction = model.predict([features]).tolist()
+        return jsonify({"prediction": prediction})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
